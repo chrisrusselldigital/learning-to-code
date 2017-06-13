6107435dddbb2114154efd12f5c172e44d16db5d
chrisrusselldigital <chris.john.russell@gmail.com>
2017-06-13 15:24:59 +0100

    Add files via upload

--- scarlet.rb
+++ scarlet.rb
@@ -1,10 +1,18 @@
-print "Hi Scarlet! How old are you?"
+print "Hello. I'm your computer. What's your name?\n"
+name = gets.chomp.capitalize
+print "Hi #{name}! How old are you?\n"
 age = gets.chomp
 print "so you're #{age}\n"
-print "Tell me Scarlet, what's it like being #{age}?"
+print "Tell me #{name}, what's it like being #{age}?\n"
 good_or_bad = gets.chomp
-if good_or_bad == "good"
+if good_or_bad == "good" || good_or_bad == "cool"
   print "that's nice to hear\n"
 else
-  print "oh, I'm sorry to hear that. :-("
+  print "oh, I'm sorry to hear that. :-(\n"
+end
+print "Do you have any brothers or sisters?\n"
+siblings = gets.chomp
+if siblings == "yes"
+  print "oh cool\n"
+else "oh ok."
 end
