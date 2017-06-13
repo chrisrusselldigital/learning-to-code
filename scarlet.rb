512f5f2db85c12ea5c384a0300e7b263d09cc0a9
chrisrusselldigital <chris.john.russell@gmail.com>
2017-06-13 14:27:19 +0100

    Add files via upload

--- scarlet.rb
+++ scarlet.rb
@@ -1,3 +1,10 @@
 print "Hi Scarlet! How old are you?"
 age = gets.chomp
-print "so you're #{age}"
+print "so you're #{age}\n"
+print "Tell me Scarlet, what's it like being #{age}?"
+good_or_bad = gets.chomp
+if good_or_bad == "good"
+  print "that's nice to hear\n"
+else
+  print "oh, I'm sorry to hear that. :-("
+end
