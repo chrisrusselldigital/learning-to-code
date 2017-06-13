be9f949f47fae96872ece4252bd50884f5881d03
chrisrusselldigital <chris.john.russell@gmail.com>
2017-06-13 16:25:06 +0100

    Add files via upload

--- scarlet.rb
+++ scarlet.rb
@@ -1,4 +1,6 @@
-print "Hello. I'm your computer. What's your name?\n"
+print "Hello.\n"
+print "I'm your computer.\n"
+print "What's your name?\n"
 name = gets.chomp.capitalize
 print "Hi #{name}! How old are you?\n"
 age = gets.chomp
@@ -14,5 +16,19 @@ print "Do you have any brothers or sisters?\n"
 siblings = gets.chomp
 if siblings == "yes"
   print "oh cool\n"
-else "oh ok."
+else "oh ok.\n"
+end
+print "#{name}, what's your favourite subject at school?\n"
+school_subject = gets.chomp.capitalize
+print "Wow, you like #{school_subject}\n"
+print "That's cool!\n"
+print "#{name}, would you like to see me count to ten?\n"
+shall_I_count_to_ten = gets.chomp
+if shall_I_count_to_ten == "yes"
+print "oh great...here goes\n"
+for one_to_ten in 1..10
+  puts one_to_ten
+end
+else
+  print "oh, ok, I won't."
 end
