78e2422275074bc09490099d3666a32bf9a0d1c8
chrisrusselldigital <chris.john.russell@gmail.com>
2017-07-03 15:05:19 +0100

    Add files via upload

---
+++ your_name.rb
@@ -0,0 +1,11 @@
+puts "What's your first name?"
+first = gets.chomp.capitalize
+
+puts "What's your middle name?"
+middle = gets.chomp.capitalize
+
+puts "What's your last name?"
+last = gets.chomp.capitalize
+
+puts "So, your full name is \n"
+puts "#{first} #{middle} #{last}"
