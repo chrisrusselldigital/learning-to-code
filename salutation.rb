16de5b89d24a508e32d67d599defaa695008f0df
chrisrusselldigital <chris.john.russell@gmail.com>
2017-07-05 20:51:49 +0100

    Add files via upload

--- salutation.rb
+++ salutation.rb
@@ -5,6 +5,10 @@ puts salutation[1]
 puts salutation[0]
 puts salutation[3]
 
+#The syntax is variable.each do |new-variable\
+# ---thing you want to happen----
+#end
+
 salutation.each do |greeting|
   puts "Here's a thing you could say: #{greeting}"
 end
