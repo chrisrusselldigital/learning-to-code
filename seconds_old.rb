654546b14320e76367160955429740a2f967618c
chrisrusselldigital <chris.john.russell@gmail.com>
2017-07-06 14:02:03 +0100

    Add files via upload

---
+++ seconds_old.rb
@@ -0,0 +1,15 @@
+minutes = 1246000000/60
+
+puts "You are #{minutes} minutes old."
+
+hours = minutes/60
+
+puts "You are #{hours} hours old."
+
+days = hours/24
+
+puts "You are #{days} days old."
+
+years = days/365
+
+puts "You are #{years} years old."
